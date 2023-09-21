d31440d6e951c7d21e48e39f2f19401be771e3f7
NathanBnvn <bienvenu.nathan72@gmail.com>
2023-09-21 12:25:37 +0200

    fix & optimizing pie chart

--- belib_graph/main.cpp
+++ belib_graph/main.cpp
@@ -13,15 +13,94 @@ using json = nlohmann::json;
 //
 
 int maxCount;
-gdImagePtr chart;
+
 int white, green, yellow, red, sunny_blue, grey, navy, pale_yellow, dark_purple, black, pink, olivine,
     cadet_grey, tyrian_purple, ash_gray, light_coral, plum, rose_ebony, sienna, cam_blue, brigde_blue;
 
-void create_chart(gdImagePtr chart, int countByDepartement[20])
-{}
+
+//void create_chart(gdImagePtr chart, int countByDepartement[20])
+//{}
+
+void create_chart(int countByArrondissements[20])
+{
+
+    FILE *png_file;
+    gdImagePtr chart;
+    chart = gdImageCreate(500, 500);
+
+    white = gdImageColorAllocate(chart, 255, 255, 255);
+
+    black = gdImageColorAllocate(chart, 0, 0, 0);
+    yellow = gdImageColorAllocate(chart, 255, 178, 0);
+    green = gdImageColorAllocate(chart, 110, 178, 0);
+    grey = gdImageColorAllocate(chart, 130, 130, 130);
+    navy = gdImageColorAllocate(chart, 0, 42, 111);
+    pale_yellow = gdImageColorAllocate(chart, 255, 186, 73);
+    sunny_blue = gdImageColorAllocate(chart, 32, 163, 158);
+    red = gdImageColorAllocate(chart, 255, 0, 0);
+    dark_purple = gdImageColorAllocate(chart, 35, 0, 30);
+    pink = gdImageColorAllocate(chart, 239, 91, 91);
+
+    cadet_grey = gdImageColorAllocate(chart, 164, 169, 173);
+    tyrian_purple = gdImageColorAllocate(chart, 74, 0, 31);
+    ash_gray = gdImageColorAllocate(chart, 167, 196, 194);
+    light_coral = gdImageColorAllocate(chart, 246, 130, 140);
+    plum = gdImageColorAllocate(chart, 160, 62, 153);
+    rose_ebony = gdImageColorAllocate(chart, 89, 56, 55);
+    sienna = gdImageColorAllocate(chart, 148, 41, 17);
+    cam_blue = gdImageColorAllocate(chart, 148, 185, 175);
+    brigde_blue = gdImageColorAllocate(chart, 144, 165, 131);
+    olivine = gdImageColorAllocate(chart, 171, 181, 87);
+
+
+    int colors[20] = {black, yellow, green, grey, navy, sunny_blue, red, pale_yellow, dark_purple, pink, cadet_grey,
+                      tyrian_purple, ash_gray, light_coral, plum, rose_ebony, sienna, cam_blue, brigde_blue, olivine};
+
+
+    gdImageSetAntiAliased(chart, gdTrueColorAlpha(255, 0, gdBlueMax, gdAlphaOpaque));
+
+    int x = 0;
+    int y;
+    char legend_txt[13];
+
+    int text_y = 20;
+    int text_x = 20;
+    int color_legend_y_2 = 30;
+    int color_legend_y = 25;
+
+    for(int c = 0; c < 20; c++){
+        if(countByArrondissements[c] > 0) {
+            if(c > 0){
+                x = x + y;
+            }
+            y = (((countByArrondissements[c]/ 19)) * 360) /100;
+            gdImageFilledArc(chart, 250, 250, 250, 250, x, y, colors[c], gdPie);
+
+        }
+
+        snprintf(legend_txt, 13, "%d%% - %d Arr.", (countByArrondissements[c]*100) / 1991, c+1);
+        gdImageFilledRectangle(chart, 10, color_legend_y, 15, color_legend_y_2, colors[c]);
+        gdImageString(chart, gdFontSmall, text_x, text_y, (unsigned char*)legend_txt, black);
+        text_y = text_y + 20;
+        color_legend_y_2 = color_legend_y_2 + 20;
+        color_legend_y = color_legend_y + 20;
+    }
+
+
+    gdImageSetAntiAliased(chart, gdTrueColorAlpha(255, 0, gdBlueMax, gdAlphaOpaque));
+    //x = y;
+
+    png_file = fopen("../belib_graph/test.png", "wb");
+
+    gdImagePng(chart, png_file);
+    fclose(png_file);
+
+    gdImageDestroy(chart);
+}
 
 int main()
 {
+
     ifstream file("../source/belib.json");
     json data = json::parse(file);
     maxCount = end(data) - begin(data);
@@ -30,7 +109,6 @@ int main()
     int arrondissements[maxCount];
     for (int i = 0; i < maxCount; i++) {
         if(!data[i]["adresse_station"].empty()){
-            //cout << data[i]["arrondissement"].get<std::string>() << "\n";
             string str3 = data[i]["adresse_station"].get<std::string>();
 
             str3.erase(str3.begin(), str3.end()-11);
@@ -109,135 +187,14 @@ int main()
 
     int countByArrondissements[20] = {a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t};
 
-    FILE *png_file;
-    chart = gdImageCreate(500, 500);
-
-    white = gdImageColorAllocate(chart, 255, 255, 255);
-
-    black = gdImageColorAllocate(chart, 0, 0, 0);
-    yellow = gdImageColorAllocate(chart, 255, 178, 0);
-    green = gdImageColorAllocate(chart, 110, 178, 0);
-    grey = gdImageColorAllocate(chart, 130, 130, 130);
-    navy = gdImageColorAllocate(chart, 0, 42, 111);
-    pale_yellow = gdImageColorAllocate(chart, 255, 186, 73);
-    sunny_blue = gdImageColorAllocate(chart, 32, 163, 158);
-    red = gdImageColorAllocate(chart, 255, 0, 0);
-    dark_purple = gdImageColorAllocate(chart, 35, 0, 30);
-    pink = gdImageColorAllocate(chart, 239, 91, 91);
-
-    cadet_grey = gdImageColorAllocate(chart, 164, 169, 173);
-    tyrian_purple = gdImageColorAllocate(chart, 74, 0, 31);
-    ash_gray = gdImageColorAllocate(chart, 167, 196, 194);
-    light_coral = gdImageColorAllocate(chart, 246, 130, 140);
-    plum = gdImageColorAllocate(chart, 160, 62, 153);
-    rose_ebony = gdImageColorAllocate(chart, 89, 56, 55);
-    sienna = gdImageColorAllocate(chart, 148, 41, 17);
-    cam_blue = gdImageColorAllocate(chart, 148, 185, 175);
-    brigde_blue = gdImageColorAllocate(chart, 144, 165, 131);
-    olivine = gdImageColorAllocate(chart, 171, 181, 87);
-
-
-    int colors[20] = {black, yellow, green, grey, navy, sunny_blue, red, pale_yellow, dark_purple, pink, cadet_grey,
-                      tyrian_purple, ash_gray, light_coral, plum, rose_ebony, sienna, cam_blue, brigde_blue, olivine};
-
-
-    gdImageSetAntiAliased(chart, gdTrueColorAlpha(255, 0, gdBlueMax, gdAlphaOpaque));
-
-    int x = 0;
-    int y;
-    char legend_txt[13];
-
-    int text_y = 20;
-    int text_x = 20;
-    int color_legend_y_2 = 30;
-    int color_legend_y = 25;
-
-    for(int c = 0; c < 20; c++){
-        if(countByArrondissements[c] > 0) {
-            if(c > 0){
-                x = x + y;
-            }
-            y = (((countByArrondissements[c]/ 19)) * 360) /100;
-            gdImageFilledArc(chart, 250, 250, 250, 250, x, y, colors[c], gdPie);
-
-        }
-
-        snprintf(legend_txt, 13, "%d%% - %d Arr.", (countByArrondissements[c]*100) / 1991, c+1);
-        gdImageFilledRectangle(chart, 10, color_legend_y, 15, color_legend_y_2, colors[c]);
-        gdImageString(chart, gdFontSmall, text_x, text_y, (unsigned char*)legend_txt, black);
-        text_y = text_y + 20;
-        color_legend_y_2 = color_legend_y_2 + 20;
-        color_legend_y = color_legend_y + 20;
-    }
-
-
-    gdImageSetAntiAliased(chart, gdTrueColorAlpha(255, 0, gdBlueMax, gdAlphaOpaque));
-    //x = y;
-
-    png_file = fopen("test.png", "wb");
-
-    gdImagePng(chart, png_file);
-    fclose(png_file);
-
-    gdImageDestroy(chart);
-
-    //create_chart(chart, &countByDepartement[20]);
-    //int * percentage = nullptr;
-
+    create_chart(countByArrondissements);
 
     return 0;
 }
 
+
+
 /*
-void create_chart(gdImagePtr chart)
-{
-
-    FILE *png_file;
-
-    white = gdImageColorAllocate(chart, 255, 255, 255);
-
-    black = gdImageColorAllocate(chart, 0, 0, 0);
-    yellow = gdImageColorAllocate(chart, 255, 178, 0);
-    green = gdImageColorAllocate(chart, 110, 178, 0);
-    grey = gdImageColorAllocate(chart, 130, 130, 130);
-    navy = gdImageColorAllocate(chart, 0, 42, 111);
-    red = gdImageColorAllocate(chart, 255, 0, 0);
-
-    //gdImageFilledArc(chart, 250, 250, 250, 250, 0, 88, yellow, gdPie);
-    //gdImageArc(chart, 250, 250, 250, 250, 0, 88, black);
-
-    //gdImageFilledArc(chart, 250, 250, 250, 250, 88, 91, navy, gdPie);
-    //gdImageFillToBorder(chart, 88, 250, 12, black);
-
-    //gdImageFilledArc(chart, 250, 250, 250, 250, 91, 120, grey, gdPie);
-    //gdImageArc(chart, 250, 250, 250, 250, 91, 120, black);
-
-    //gdImageFilledArc(chart, 250, 250, 250, 250, 120, 180 , green, gdPie);
-    //gdImageArc(chart, 250, 250, 250, 250, 120, 180, black);
-
-    //gdImageFilledArc(chart, 250, 250, 250, 250, 180, 360, red, gdPie);
-    //gdImageArc(chart, 250, 250, 250, 250, 180, 360, black);
-
-    //gdImageLine(chart, 250, 250, 375, 250, black);
-    //gdImageLine(chart, 250, 250, 250+4, 375, black);
-
-    gdImageSetAntiAliased(chart, gdTrueColorAlpha(255, 0, gdBlueMax, gdAlphaOpaque));
-
-    //gdImageString(chart, gdFontSmall, 250, 250, "blaise", black);
-    //gdImageString(chart, gdFontSmall, 285+40, 285+40, "balaise", black);
-    //gdImageString(chart, gdFontSmall, 285+90, 285+90, "le blaise", black);
-
-    //gdImageDashedLine();
-    //chart = gdImageScale(chart, 500, 500);
-
-    png_file = fopen("test.png", "wb");
-
-    gdImagePng(chart, png_file);
-    fclose(png_file);
-
-    gdImageDestroy(chart);
-}
-
 void set_chart_value(char *name, char *percentage)
 {
     int n;
